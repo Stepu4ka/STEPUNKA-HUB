950db3a6fec016a95df66b8ee5f6c8d8c577fd40
Stepu4ka <125278943+Stepu4ka@users.noreply.github.com>
2023-02-13 17:24:01 +0300

    Update JAILBREAK HUB v1.lua

--- JAILBREAK HUB v1.lua
+++ JAILBREAK HUB v1.lua
@@ -25,3 +25,19 @@ local Section = Tab:NewSection("RedWire s")
 Section:NewButton("ACTIVE", "", function()
 	loadstring(Game:HttpGet("https://raw.githubusercontent.com/RedWirePlatinumTwo/RobloxScripts/main/JailbreakGUI.lua"))()
 end)
+
+local Tab = Window:NewTab("inf rocket fuel")
+local Section = Tab:NewSection("inf rocket fuel")
+Section:NewButton("ACTIVE", "", function()
+local mouse = game:GetService('Players').LocalPlayer:GetMouse()
+local function hotkeyHandler(key)
+  if (key == 'q') then
+    for _, v in next, getgc(true) do
+      if (type(v) == 'table' and rawget(v, 'Nitro')) then
+        v.Nitro = 250
+      end
+    end
+  end
+end
+mouse.KeyDown:connect(hotkeyHandler)
+end)
